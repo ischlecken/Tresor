3201b8ff9d3fd738a7f8eaf07a36c32ec40c197c
Hugo Schlecken <h.schlecken@gmx.de>
2015-05-24 11:04:48 +0200

    add unittest for vault

--- TresorTests/TresorTests.m
+++ TresorTests/TresorTests.m
@@ -9,28 +9,84 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+BOOL gInited = FALSE;
+
+#define kVaultName @"vault-test"
+#define kVaultType @"vault-type"
+
 @interface TresorTests : XCTestCase
 
 @end
 
 @implementation TresorTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+/*
+ *
+ */
+-(void) setUp
+{ [super setUp];
+  
+  if( !gInited )
+  { gInited = TRUE;
+    
+    NSError* error = nil;
+
+    _TRESORCONFIG.databaseStoreName = @"testcase.sqlite";
+    [[TresorFileUtil sharedInstance] deleteFileURL:[_TRESORCONFIG databaseStoreURL] didFailWithError:&error];
+    [_TRESORMODEL resetCoreDataObjects];
+  } /* of if */
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+/*
+ *
+ */
+-(void) tearDown
+{
+  [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+
+/*
+ *
+ */
+-(void) test01CreateVault
+{ XCTestExpectation* expection = [self expectationWithDescription:@"Should be a Vault object."];
+  
+  VaultParameter* vp = [VaultParameter new];
+  vp.name = kVaultName;
+  vp.type = kVaultType;
+  
+  PMKPromise* vaultPromise = [Vault vaultObjectWithParameter:vp];
+  
+  vaultPromise.then(^(Vault* vault)
+  { XCTAssert([vault isKindOfClass:[Vault class]], @"Should be a Vault object.");
+    
+    [expection fulfill];
+  });
+  
+  [self waitForExpectationsWithTimeout:5 handler:nil];
 }
 
-- (void)testPerformanceExample {
+
+/*
+ *
+ */
+- (void)test02FindVault
+{ NSError* error = nil;
+  
+  Vault* v = [Vault findVaultByName:kVaultName andError:&error];
+  
+  XCTAssertNotNil(v,@"Vault %1$@ not found, error is %2$@",kVaultName,error);
+  
+  XCTAssertTrue([v.vaultname isEqualToString:kVaultName],@"Wrong vaultname");
+  XCTAssertTrue([v.vaulttype isEqualToString:kVaultType],@"Wrong vaulttype");
+}
+
+/*
+ *
+ */
+- (void)test99PerformanceExample
+{
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
